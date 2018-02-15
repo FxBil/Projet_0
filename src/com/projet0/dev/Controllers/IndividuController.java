56036b0bfab21877c6b7ea3453096afa910e586a
loujulien <laetitia_julien@live.fr>
2018-02-15 12:35:49 +0100

    [ADD]getGenre, getAge, getPoids, getTaille,getConstitution, getForce, getAgilite, getPerception, getCharisme, getIntelligence, getEtat | setGenre, setAge, setPoids, setTaille,setConstitution, setForce, setAgilite, setPerception, setCharisme, setIntelligence, setEtat dans IndividuController

--- src/com/projet0/dev/Controllers/IndividuController.java
+++ src/com/projet0/dev/Controllers/IndividuController.java
@@ -8,6 +8,120 @@ public abstract class IndividuController implements ActionListener {
 	int genre, age, poids, taille,constitution, force, agilite, perception, charisme, intelligence, etat;
 	String nom, prenom;
 	Image image;
+	
+	public int getGenre(){  
+	    return genre;
+	 }
+	
+	public int getAge(){  
+	    return age;
+	 }
+	
+	public int getPoids(){  
+	    return poids;
+	 }
+	
+	public int getTaille(){  
+	    return taille;
+	 }
+	
+	public int getConstitution(){  
+	    return constitution;
+	 }
+	
+	public int getForce(){  
+	    return force;
+	 }
+	
+	public int getAgilitee(){  
+	    return agilite;
+	 }
+	
+	public int getPerception(){  
+	    return perception;
+	 }
+	
+	public int getCharisme(){  
+	    return charisme;
+	 }
+	
+	public int getIntelligence(){  
+	    return intelligence;
+	 }
+	
+	public int getEtat(){  
+	    return etat;
+	 }
+	
+	public String getNom(){  
+	    return nom;
+	 }
+
+	public String getPrenom(){  
+	    return prenom;
+	 }
+	
+
+	public Image getImage(){  
+	    return image;
+	 }
+	
+	 public void setGenre(int aGenre){
+	    genre = aGenre;
+	 }
+	 
+	 public void setAge(int aAge){
+		 age = aAge;
+	 }
+	 
+	 public void setPoids(int aPoids){
+		 poids = aPoids;
+	 }
+	 
+	 public void setTaille(int aTaille){
+		 taille = aTaille;
+	 }
+	 
+	 public void setConstitution(int aConstitution){
+		 constitution = aConstitution;
+	 }
+	 
+	 public void setForce(int aForce){
+		 force = aForce;
+	 }
+	 
+	 public void setAgilite(int aAgilite){
+		 agilite = aAgilite;
+	 }
+	 
+	 public void setPerception(int aPerception){
+		 perception = aPerception;
+	 }
+	 
+	 public void setCharisme(int aCharisme){
+		 charisme = aCharisme;
+	 }
+	 
+	 public void setIntelligence(int aIntelligence){
+		 intelligence = aIntelligence;
+	 }
+	 
+	 public void setEtat(int aEtat){
+		 etat = aEtat;
+	 }
+	 
+	 public void setNomt(String aNom){
+		 nom = aNom;
+	 }
+	 
+	 public void setPrenom(String aPrenom){
+		 prenom = aPrenom;
+	 }
+	 
+	 public void setImage(Image aImage){
+		 image = aImage;
+	 }
+	 
 
 	
 	public int attaque(PersonnageController unPersonnage, PNJController unPnj, CombatController quiJoue) {
