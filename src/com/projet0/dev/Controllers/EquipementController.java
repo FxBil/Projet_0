aa69491acf62bb0b496b7fe5b436280d68638851
loujulien <laetitia_julien@live.fr>
2018-02-15 11:37:40 +0100

    [ADD]methode verification dans EquipementController

--- src/com/projet0/dev/Controllers/EquipementController.java
+++ src/com/projet0/dev/Controllers/EquipementController.java
@@ -6,6 +6,27 @@ import java.awt.event.ActionListener;
 public class EquipementController implements ActionListener {
 	
 	boolean estEquipe;
+	
+	
+	static public boolean verification(ObjetController objet){
+        boolean contain = false;
+        int type=set(objet.type);
+        int [] emplacement = ; // je ne sais pas avec quoi tester , a voir avec l'equipe
+         for(type=0;type<emplacement.length;type++)
+                {
+                    if(emplacement[type]==set(objet.type))
+                    {
+                        contain = true;
+                    }
+                }
+         if(contain)
+             {System.out.println("l’objet"+ objet.getType()+" peut être équipé");}
+         else
+             {System.out.println("l’objet"+objet.getType()+" peut pas être équipé");}
+
+        }
+
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
