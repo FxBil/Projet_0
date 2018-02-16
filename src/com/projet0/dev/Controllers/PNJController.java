185ce8687d4fa5aef6151c689d31d0343805501f
loujulien <laetitia_julien@live.fr>
2018-02-16 10:57:47 +0100

    [ADD]creation getMapMarchandise et setMapmarchandise dans PNJController

--- src/com/projet0/dev/Controllers/PNJController.java
+++ src/com/projet0/dev/Controllers/PNJController.java
@@ -7,6 +7,15 @@ import java.util.HashMap;
 public class PNJController extends IndividuController implements ActionListener {
 	
     HashMap<Integer, String> mapMarchandise = new HashMap<Integer, String>();
+    
+    
+    public HashMap<Integer,String> getMapMarchandise(){  
+	    return mapMarchandise;
+	 }
+    
+    public void setMapMarchandise(HashMap<Integer,String> aMapMarchandise){
+	    mapMarchandise = aMapMarchandise;
+	 }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
