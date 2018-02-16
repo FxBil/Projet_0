91904acac282c19d2806f75c7d71a2e594e55997
loujulien <laetitia_julien@live.fr>
2018-02-16 11:02:15 +0100

    [ADD]getPersonnage , getPnj et setPersonnage , setPnj dans RencontreController

--- src/com/projet0/dev/Controllers/RecontreController.java
+++ src/com/projet0/dev/Controllers/RecontreController.java
@@ -4,7 +4,23 @@ import java.awt.event.ActionListener;
 
 public class RecontreController extends EvenementController implements ActionListener {
 	
-	PersonnageController  personnage;
-    PNJController  pnj;
+	PersonnageController personnage;
+    PNJController pnj;
+    
+    public PersonnageController getPersonnage(){  
+	    return personnage;
+	 }
+    
+    public PNJController getPnj(){  
+	    return pnj;
+	 }
+	
+	 public void setPersonnage(PersonnageController aPersonnage){
+	    personnage = aPersonnage;
+	 }
+
+	 public void setPnj(PNJController aPnj){
+		    pnj = aPnj;
+		 }
 
 }
